18b433fe6d6fa2353848ec607c2bb34930a1f743
Albin Smajli <64081932+AlbinSmajli22@users.noreply.github.com>
2023-08-31 00:20:55 +0200

    .
    ,

--- pages/directory.php
+++ pages/directory.php
@@ -73,9 +73,34 @@ $currentTime = date('h:i A');
     <!--<button  class="addFilter">
       <p>Filters</p>
     </button>-->
-    <a class="addFilter" data-target="#filterModal" data-toggle="modal">
-      Filters
-    </a>
+    <button type="button" class="addFilter" data-bs-toggle="modal" data-bs-target="#filtersModal" data-bs-whatever="@mdo">Filters</button>
+
+    <div class="modal fade" id="filtersModal" tabindex="-1" aria-labelledby="filtersModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="filtersleModalLabel">New message</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <form>
+          <div class="mb-3">
+            <label for="recipient-name" class="col-form-label">Recipient:</label>
+            <input type="text" class="form-control" id="recipient-name">
+          </div>
+          <div class="mb-3">
+            <label for="message-text" class="col-form-label">Message:</label>
+            <textarea class="form-control" id="message-text"></textarea>
+          </div>
+        </form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Send message</button>
+      </div>
+    </div>
+  </div>
+</div>
    </div>
    <form class="serachBar" method="POST">
     <input type="text" name="search-box" class="search-box" placeholder="Search directory...">
